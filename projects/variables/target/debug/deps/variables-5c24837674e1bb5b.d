6e894c757c288bc9785866740db62f2e00baaaaf
lozlof <lozlofcyber@gmail.com>
2024-10-31 21:26:30 +0000

    Created variables project

---
+++ projects/variables/target/debug/deps/variables-5c24837674e1bb5b.d
@@ -0,0 +1,5 @@
+/home/vscodeusr/Rust-Loz/projects/variables/target/debug/deps/variables-5c24837674e1bb5b: src/main.rs
+
+/home/vscodeusr/Rust-Loz/projects/variables/target/debug/deps/variables-5c24837674e1bb5b.d: src/main.rs
+
+src/main.rs:

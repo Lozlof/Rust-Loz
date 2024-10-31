6815afc536a45ef74d94a7d205df0387ba32b141
lozlof <lozlofcyber@gmail.com>
2024-10-31 17:03:39 +0000

    Add string methods notes

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,6 +1,7 @@
 use std::io; // To obtain user input and then print the result as output, we need to bring the io input/output library into scope.
 use rand::Rng; // First we add the line use rand::Rng;. The Rng trait defines methods that random number generators implement, and this trait must be in scope for us to use those methods.
-use std::cmp::Ordering;
+use std::cmp::Ordering; /* The Ordering type is another enum and has the variants Less, Greater, and Equal. 
+                           These are the three outcomes that are possible when you compare two values. */
 
 fn main () {
    println!("Guess the number!");
@@ -41,14 +42,50 @@ fn main () {
    /* Result’s variants are Ok and Err. 
       The Ok variant indicates the operation was successful, and inside Ok is the successfully generated value. 
       The Err variant means the operation failed, and Err contains information about how or why the operation failed. */
+  
+   /* We create a variable named guess. But wait, doesn’t the program already have a variable named guess? It does, but helpfully Rust allows us to shadow the previous value of guess with a new one. 
+      Shadowing lets us reuse the guess variable name rather than forcing us to create two unique variables, such as guess_str and guess, for example.
+      Know that this feature is often used when you want to convert a value from one type to another type. */
+   let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
    // This line prints the string that now contains the user’s input. The {} set of curly brackets is a placeholder.
    println!("You guessed: {}", guess);
 
+   /* The cmp method compares two values and can be called on anything that can be compared. 
+      It takes a reference to whatever you want to compare with: here it’s comparing guess to secret_number. 
+      Then it returns a variant of the Ordering enum we brought into scope with the use statement. 
+      We use a match expression to decide what to do next based on which variant of Ordering was returned from the call to cmp with the values in guess and secret_number. */
    match guess.cmp(&secret_number) {
       Ordering::Less => println!("Too small!"),
       Ordering::Greater => println!("Too big!"),
       Ordering::Equal => println!("You win!"),
+   }
+   /* A match expression is made up of arms. 
+      An arm consists of a pattern to match against, and the code that should be run if the value given to match fits that arm’s pattern. 
+      Rust takes the value given to match and looks through each arm’s pattern in turn. 
+      Patterns and the match construct are powerful Rust features: they let you express a variety of situations your code might encounter and they make sure you handle them all. */
+   /* vscodeusr@lozcloud:~/Rust-Loz/projects/guessing_game/src$ cargo run
+      Compiling guessing_game v0.1.0 (/home/vscodeusr/Rust-Loz/projects/guessing_game)
+      error[E0308]: mismatched types
+      --> src/main.rs:53:20
+      |
+      53    |    match guess.cmp(&secret_number) {
+      |                --- ^^^^^^^^^^^^^^ expected `&String`, found `&{integer}`
+      |                |
+      |                arguments to this method are incorrect
+      |
+      = note: expected reference `&String`
+              found reference `&{integer}`
+      note: method defined here
+      --> /rustc/f6e511eec7342f59a25f7c0534f1dbea00d01b14/library/core/src/cmp.rs:838:8
+
+      For more information about this error, try `rustc --explain E0308`.
+      error: could not compile `guessing_game` (bin "guessing_game") due to 1 previous error */
+   /* The core of the error states that there are mismatched types. 
+      Rust has a strong, static type system. However, it also has type inference. When we wrote let mut guess = String::new(), Rust was able to infer that guess should be a String and didn’t make us write the type. 
+      The secret_number, on the other hand, is a number type. 
+      A few of Rust’s number types can have a value between 1 and 100: i32, a 32-bit number; u32, an unsigned 32-bit number; i64, a 64-bit number; as well as others. Unless otherwise specified, Rust defaults to an i32, which is the type of secret_number unless you add type information elsewhere that would cause Rust to infer a different numerical type. 
+      The reason for the error is that Rust cannot compare a string and a number type. */
 }
 
 // Rust doesn’t yet include random number functionality in its standard library. However, the Rust team does provide a rand crate with said functionality.
